65e847d6458e2c768554841b61e36480d75b82ef
chLi <565353780@qq.com>
2025-09-22 19:04:19 +0800

    finish fast query polygon algo

--- sweep_polygon/Data/polygon2d.py
+++ sweep_polygon/Data/polygon2d.py
@@ -1,6 +1,7 @@
 import numpy as np
 import open3d as o3d
 from typing import Union
+from scipy.interpolate import interp1d
 
 from sweep_polygon.Method.render import createNormalLineSet, getPcd
 
@@ -12,7 +13,7 @@ class Polygon2D(object):
         self.vertices = vertices
 
         if position is None:
-            self.position = np.zeros(3)  # [x,y,z]
+            self.position = np.zeros(3)
         else:
             position = np.asarray(position)
             if position.shape != (3,):
@@ -21,8 +22,12 @@ class Polygon2D(object):
 
         self.normals = self.compute_vertex_normals()
 
-        self.vt = np.empty(0)  # 顶点对应的参数化t
-        self.update()
+        self.vn = np.hstack([self.vertices, self.normals])
+
+        self.vt = self.compute_vertex_length_t()
+
+        self.createLinearQueryFunc()
+        return
 
     @property
     def vertices3d(self) -> np.ndarray:
@@ -78,13 +83,7 @@ class Polygon2D(object):
         y = verts[:, 1]
         return np.sum((x[:-1] * y[1:] - x[1:] * y[:-1])) > 0
 
-    def center(self) -> np.ndarray:
-        # 用顶点均值作为中心，再加上位置偏移（x,y,z）
-        local_center = np.mean(self.vertices, axis=0)  # (x,y)
-        global_center = np.array([local_center[0], local_center[1], 0]) + self.position
-        return global_center
-
-    def update(self) -> bool:
+    def compute_vertex_length_t(self) -> np.ndarray:
         # 计算每条边长度
         verts_ext = np.vstack([self.vertices, self.vertices[0]])  # 尾部加头部闭合
         edges = verts_ext[1:] - verts_ext[:-1]
@@ -95,96 +94,25 @@ class Polygon2D(object):
         total_length = cum_length[-1]
 
         # 顶点对应的归一化参数t（首顶点t=0）
-        self.vt = np.hstack(([0], cum_length[:-1])) / total_length
+        vt = np.hstack(([0], cum_length[:-1])) / total_length
+        return vt
 
+    def createLinearQueryFunc(self) -> bool:
+        expand_vn = np.vstack([self.vn, self.vn[0, :]])
+        expand_vt = np.hstack([self.vt, [1.0]])
+        self.query_func = interp1d(
+            expand_vt, expand_vn, kind="linear", axis=0, fill_value="extrapolate"
+        )
         return True
 
-    def queryPoints(self, t: np.ndarray) -> np.ndarray:
-        # 参数t归一化到[0,1)
-        t = np.mod(t, 1.0)
+    def query(self, t: np.ndarray) -> np.ndarray:
+        query_vn = self.query_func(t)
 
-        # 为方便插值，把vt和vertices闭合一圈
-        vt_ext = np.hstack((self.vt, 1.0))
-        verts_ext = np.vstack([self.vertices, self.vertices[0]])
-
-        result_points = []
-        for val in t:
-            # 找到参数val对应的边索引
-            idx = np.searchsorted(vt_ext, val) - 1
-            idx = np.clip(idx, 0, len(self.vt) - 1)
-
-            # 计算插值比例
-            segment_length = vt_ext[idx + 1] - vt_ext[idx]
-            if segment_length == 0:
-                # 处理特殊情况，边长为0，直接取顶点
-                pt_2d = verts_ext[idx]
-            else:
-                local_t = (val - vt_ext[idx]) / segment_length
-                pt_2d = (1 - local_t) * verts_ext[idx] + local_t * verts_ext[idx + 1]
-
-            # 转成三维点，z分量来自position的第三个分量
-            pt_3d = np.array([pt_2d[0], pt_2d[1], 0]) + self.position
-            result_points.append(pt_3d)
-
-        return np.array(result_points)
-
-    def queryNormals(self, t: np.ndarray) -> np.ndarray:
-        t = np.mod(t, 1.0)
-
-        vt_ext = np.hstack((self.vt, 1.0))
-        verts_ext = np.vstack([self.vertices, self.vertices[0]])
-
-        normals = []
-
-        for val in t:
-            # 找到对应的边索引
-            idx = np.searchsorted(vt_ext, val) - 1
-            idx = np.clip(idx, 0, len(self.vt) - 1)
-
-            # 判断是否与某个顶点重合（用一个容差，比如1e-8）
-            close_to_vertex = np.isclose(val, self.vt, atol=1e-8)
-            if np.any(close_to_vertex):
-                vertex_idx = np.argmax(close_to_vertex)
-                n = len(self.vertices)
-
-                # 计算前一条边方向向量
-                prev_idx = (vertex_idx - 1) % n
-                edge_prev = verts_ext[vertex_idx] - verts_ext[prev_idx]
-                edge_prev /= np.linalg.norm(edge_prev)
-
-                # 计算后一条边方向向量
-                next_idx = vertex_idx
-                edge_next = verts_ext[next_idx + 1] - verts_ext[next_idx]
-                edge_next /= np.linalg.norm(edge_next)
-
-                # 平均方向
-                direction = edge_prev + edge_next
-                norm = np.linalg.norm(direction)
-                if norm < 1e-12:
-                    # 极端情况，前后方向完全相反，直接用前方向
-                    direction = edge_prev
-                else:
-                    direction /= norm
-            else:
-                # 在边上，方向是该边向量
-                edge = verts_ext[idx + 1] - verts_ext[idx]
-                norm = np.linalg.norm(edge)
-                if norm == 0:
-                    # 边长为0，直接取零向量（或者其他处理）
-                    direction = np.array([0.0, 0.0])
-                else:
-                    direction = edge / norm
-
-            # 计算法线向量 [-y, x]
-            normal_2d = np.array([-direction[1], direction[0]])
-            normal_2d /= np.linalg.norm(normal_2d)  # 保证单位向量
-
-            # 转换成三维，z=0，考虑位置偏移
-            normal_3d = np.array([normal_2d[0], normal_2d[1], 0.0])
-
-            normals.append(normal_3d)
-
-        return np.array(normals)
+        expand_vn = np.zeros([query_vn.shape[0], 6])
+        expand_vn[:, :2] = query_vn[:, :2]
+        expand_vn[:, :3] += self.position
+        expand_vn[:, 3:5] = query_vn[:, 2:]
+        return expand_vn
 
     def renderVertices(self, normal_length: float = 0.01) -> bool:
         vertices = self.vertices3d
--- sweep_polygon/Test/polygon2d.py
+++ sweep_polygon/Test/polygon2d.py
@@ -1,7 +1,8 @@
 import numpy as np
+import open3d as o3d
 
 from sweep_polygon.Data.polygon2d import Polygon2D
-from sweep_polygon.Method.render import renderPoints
+from sweep_polygon.Method.render import createNormalLineSet, getPcd
 
 
 def generate_ellipse_polygon(a, b, num_points=100):
@@ -26,10 +27,17 @@ def test():
     # 均匀采样 t 并查询坐标
     ts = np.linspace(0, 1, 100, endpoint=False)
 
-    sampled_points1 = polygon1.queryPoints(ts)
-    sampled_points2 = polygon2.queryPoints(ts)
-    sampled_points3 = polygon3.queryPoints(ts)
+    vn1 = polygon1.query(ts)
+    vn2 = polygon2.query(ts)
+    vn3 = polygon3.query(ts)
 
-    merge_pts = np.vstack([sampled_points1, sampled_points2, sampled_points3])
+    merge_vn = np.vstack([vn1, vn2, vn3])
 
-    renderPoints(merge_pts)
+    merge_v = merge_vn[:, :3]
+    merge_n = merge_vn[:, 3:]
+
+    merge_pcd = getPcd(merge_v, merge_n)
+
+    merge_normals = createNormalLineSet(merge_v, merge_n, length=0.1)
+
+    o3d.visualization.draw_geometries([merge_pcd, merge_normals])
